432c5c757063ddeae33d120cf3c683d8b590d606
Seungkyu-Han <trust1204@gmail.com>
2024-12-02 21:42:16 +0900

    ENH: user core module dependencies

--- user/core/build.gradle.kts
+++ user/core/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
-    kotlin("jvm")
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.3.5"
+    id("io.spring.dependency-management") version "1.1.6"
 }
 
 group = "vp"
@@ -10,7 +13,27 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    //USER PERSISTENCE
+    implementation(project(":user:persistence"))
+
+    //WEBFLUX
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+    //MONGODB
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
+
+
+    //TEST
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
+    testImplementation("io.projectreactor:reactor-test")
+    testImplementation ("org.mockito:mockito-core")
+
+    //COROUTINE
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-rx3")
+
 }
 
 tasks.test {
