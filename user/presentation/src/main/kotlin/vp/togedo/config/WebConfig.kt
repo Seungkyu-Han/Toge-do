79f4fdb5381cb4e5da5fbe87bd94ce9e624509b0
Seungkyu-Han <trust1204@gmail.com>
2024-12-05 14:09:38 +0900

    ENH: webConfig to cors and resource mapping

--- user/presentation/src/main/kotlin/vp/togedo/config/WebConfig.kt
+++ user/presentation/src/main/kotlin/vp/togedo/config/WebConfig.kt
@@ -1,16 +1,12 @@
 package vp.togedo.config
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.config.CorsRegistry
 import org.springframework.web.reactive.config.ResourceHandlerRegistry
 import org.springframework.web.reactive.config.WebFluxConfigurer
 
 @Configuration
-class WebConfig(
-    @Value("\${IMAGE.PATH}")
-    private val imagePath: String,
-): WebFluxConfigurer{
+class WebConfig: WebFluxConfigurer{
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
