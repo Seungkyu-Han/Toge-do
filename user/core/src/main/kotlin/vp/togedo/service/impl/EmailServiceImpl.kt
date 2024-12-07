6e7f6f7c88ed171bb6fbadf443b0ba305fe2d2ad
Seungkyu-Han <trust1204@gmail.com>
2024-12-07 14:44:52 +0900

    TEST: email service test

--- user/core/src/main/kotlin/vp/togedo/service/impl/EmailServiceImpl.kt
+++ user/core/src/main/kotlin/vp/togedo/service/impl/EmailServiceImpl.kt
@@ -1,6 +1,6 @@
 package vp.togedo.service.impl
 
-import org.springframework.data.redis.core.ReactiveRedisOperations
+import org.springframework.data.redis.core.ReactiveRedisTemplate
 import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
@@ -10,7 +10,7 @@ import vp.togedo.service.EmailService
 @Service
 class EmailServiceImpl(
     private val reactiveKafkaProducerTemplate: ReactiveKafkaProducerTemplate<String, String>,
-    private val reactiveRedisOperations: ReactiveRedisOperations<String, String>
+    private val reactiveRedisTemplate: ReactiveRedisTemplate<String, String>
 ): EmailService {
 
     private val sendEmailValidationCodeTopic = "SEND_EMAIL_VALIDATION_CODE_TOPIC"
@@ -20,7 +20,7 @@ class EmailServiceImpl(
 
 
     override fun checkValidEmail(email: String, code: String): Mono<Boolean> =
-        reactiveRedisOperations
+        reactiveRedisTemplate
             .opsForValue().get("email:validation:$email")
             .map { it == code }
             .defaultIfEmpty(false)
