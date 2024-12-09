3f859015a22396465608f8cc31101ded6490a980
Seungkyu-Han <trust1204@gmail.com>
2024-12-09 21:21:19 +0900

    REFACTOR: extract id component

---
+++ user/presentation/src/main/kotlin/vp/togedo/config/IdConfig.kt
@@ -0,0 +1,21 @@
+package vp.togedo.config
+
+import org.bson.types.ObjectId
+import org.springframework.stereotype.Component
+import vp.togedo.util.error.errorCode.ErrorCode
+import vp.togedo.util.error.exception.UserException
+import java.util.regex.PatternSyntaxException
+
+@Component
+class IdConfig {
+
+    fun objectIdProvider(id: String) :ObjectId {
+        return try {
+            ObjectId(id)
+        }catch(illegalArgumentException: IllegalArgumentException){
+            throw UserException(ErrorCode.INVALID_TOKEN)
+        }catch(patternSyntaxException: PatternSyntaxException){
+            throw UserException(ErrorCode.INVALID_TOKEN)
+        }
+    }
+}
