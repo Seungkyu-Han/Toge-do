e76b263d4c56d3d72e71fbb1dd48b881725d9349
Seungkyu-Han <trust1204@gmail.com>
2024-12-10 21:01:41 +0900

    ENH: friend exception

--- user/core/src/main/kotlin/vp/togedo/service/FriendService.kt
+++ user/core/src/main/kotlin/vp/togedo/service/FriendService.kt
@@ -12,5 +12,7 @@ interface FriendService {
 
     fun requestFriend(userId: ObjectId, friendUserDocument: UserDocument): Mono<UserDocument>
 
+    fun acceptFriendRequest(userId: ObjectId, friendId: ObjectId): Mono<UserDocument>
+
     fun publishRequestFriendEvent(friendId: ObjectId): Mono<SenderResult<Void>>
 }
--- user/presentation/src/main/kotlin/vp/togedo/controller/exceptionHandler/ExceptionHandlerAdvice.kt
+++ user/presentation/src/main/kotlin/vp/togedo/controller/exceptionHandler/ExceptionHandlerAdvice.kt
@@ -3,15 +3,27 @@ package vp.togedo.controller.exceptionHandler
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import vp.togedo.dto.exception.FriendExceptionResDto
+import vp.togedo.util.error.exception.FriendException
 import vp.togedo.util.error.exception.UserException
 
 @RestControllerAdvice
-class ExceptionHandlerAdvice {
+class ExceptionHandlerAdvice{
 
     @ExceptionHandler(UserException::class)
     fun userExceptionHandler(userException: UserException): ResponseEntity<String> {
         return ResponseEntity
             .status(userException.errorCode.status)
             .body(userException.errorCode.message)
+    }
+
+    @ExceptionHandler(FriendException::class)
+    fun friendExceptionHandler(friendException: FriendException): ResponseEntity<FriendExceptionResDto> {
+        return ResponseEntity
+            .status(friendException.errorCode.status)
+            .body(FriendExceptionResDto(
+                state = friendException.state,
+                message = friendException.message
+            ))
     }
 }
