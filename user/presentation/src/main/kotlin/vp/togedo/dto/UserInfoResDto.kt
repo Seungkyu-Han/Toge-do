d7b625baa7c4450e2c548adc408e5f51270e2a22
Seungkyu-Han <trust1204@gmail.com>
2024-12-10 16:04:24 +0900

    REFACTOR: info dto add id

--- user/presentation/src/main/kotlin/vp/togedo/dto/UserInfoResDto.kt
+++ user/presentation/src/main/kotlin/vp/togedo/dto/UserInfoResDto.kt
@@ -3,12 +3,14 @@ package vp.togedo.dto
 import vp.togedo.document.UserDocument
 
 data class UserInfoResDto(
+    val id: String,
     val name: String?,
     val email: String?,
     val profileImageUrl: String?
 ){
     constructor(userDocument: UserDocument) :
             this(
+                id = userDocument.id!!.toString(),
                 name = userDocument.name,
                 email = userDocument.email,
                 profileImageUrl = userDocument.profileImageUrl
