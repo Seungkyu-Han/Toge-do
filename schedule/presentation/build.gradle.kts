bfb002249a5a4001800049c3140a49396ce0a04a
Seungkyu-Han <trust1204@gmail.com>
2024-12-14 20:54:54 +0900

    ENH: make schedule module

---
+++ schedule/presentation/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.3.5"
+    id("io.spring.dependency-management") version "1.1.6"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(project(":schedule:core"))
+
+    //WEBFLUX
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+    //COROUTINE
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-rx3")
+
+    //REFLECT
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    //SWAGGER
+    implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.5.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
