b0febbad8389770bf0cfe7af7cc228e611c2462a
Seungkyu-Han <trust1204@gmail.com>
2024-12-15 17:13:28 +0900

    ENH: schedule exception errorhandler

--- schedule/presentation/src/main/kotlin/vp/togedo/controller/exceptionHandler/ExceptionHandlerAdvice.kt
+++ schedule/presentation/src/main/kotlin/vp/togedo/controller/exceptionHandler/ExceptionHandlerAdvice.kt
@@ -3,6 +3,7 @@ package vp.togedo.controller.exceptionHandler
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import vp.togedo.util.error.exception.ScheduleException
 import vp.togedo.util.error.exception.UserException
 
 @RestControllerAdvice
@@ -13,6 +14,13 @@ class ExceptionHandlerAdvice {
         return ResponseEntity
             .status(userException.errorCode.status)
             .body(userException.errorCode.message)
+    }
+
+    @ExceptionHandler(ScheduleException::class)
+    fun scheduleExceptionHandler(scheduleException: ScheduleException): ResponseEntity<String> {
+        return ResponseEntity
+            .status(scheduleException.errorCode.status)
+            .body(scheduleException.errorCode.message)
     }
 
 }
