37a1e1a0281231d417a7e75f665e26bd721492ea
Seungkyu-Han <trust1204@gmail.com>
2024-12-15 10:04:58 +0900

    ENH: add fixed personal schedule document

--- schedule/persistence/src/main/kotlin/vp/togedo/document/FixedPersonalScheduleDocument.kt
+++ schedule/persistence/src/main/kotlin/vp/togedo/document/FixedPersonalScheduleDocument.kt
@@ -4,6 +4,9 @@ import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
+import reactor.core.publisher.Mono
+import vp.togedo.util.exception.ConflictScheduleException
+import vp.togedo.util.exception.InvalidTimeException
 
 @Document(collection = "fixed_personal_schedule")
 data class FixedPersonalScheduleDocument(
@@ -14,7 +17,80 @@ data class FixedPersonalScheduleDocument(
     val userId: ObjectId,
 
     var schedules: MutableList<Schedule> = mutableListOf(),
-)
+){
+    fun addSchedule(schedule: Schedule): Mono<FixedPersonalScheduleDocument> {
+        return Mono.fromCallable {
+
+            //시간 범위 검사
+            this.isValidTime(schedule)
+
+            //스케줄 충돌 체크 및 삽입 인덱스 반환
+            val insertedIndex = this.isConflictTime(schedule)
+
+            //해당 인덱스로 삽입
+            this.schedules.add(insertedIndex, schedule)
+
+            this
+        }
+    }
+
+    fun deleteScheduleById(id: ObjectId): Mono<Boolean> {
+        return Mono.fromCallable {
+            this.schedules.removeIf {
+                it.id == id
+            }
+        }
+    }
+
+    private fun isValidTime(schedule: Schedule): Boolean {
+        try{
+            validTimeCheck(schedule.startTime)
+        }catch(e: InvalidTimeException){
+            throw InvalidTimeException("시작 시간이 ${e.message}")
+        }
+        try{
+            validTimeCheck(schedule.endTime)
+        }catch(e: InvalidTimeException){
+            throw InvalidTimeException("종료 시간이 ${e.message}")
+        }
+        return true
+    }
+
+    private fun validTimeCheck(time: Int): Boolean{
+        if(time !in 10000..72359)
+            throw InvalidTimeException("week 범위 밖입니다.")
+        val hour = (time % 10000) / 100
+        if (hour !in 0..23)
+            throw InvalidTimeException("hour 범위 밖입니다.")
+        val minute = time % 100
+        if (minute !in 0..59)
+            throw InvalidTimeException("minute 범위 밖입니다.")
+        return true
+    }
+
+    private fun isConflictTime(schedule: Schedule): Int{
+
+        val index = this.schedules.binarySearch(schedule.startTime){ it.startTime }
+
+        if(index >= 0)
+            throw ConflictScheduleException("해당 시작시간에 시작하는 스케줄이 있습니다.")
+
+        val insertedIndex = (index + 1) * -1
+
+        //앞의 종료 시간과, 해당 스케줄의 시작 시간을 비교
+        if (insertedIndex > 0){
+            if(this.schedules[insertedIndex - 1].endTime > schedule.startTime)
+                throw ConflictScheduleException("전 스케줄이 종료되지 않았습니다.")
+        }
+        //뒤의 시작 시간과, 해당 스케줄의 종료 시간을 비교
+        if (insertedIndex < this.schedules.size){
+            if(this.schedules[insertedIndex].startTime < schedule.endTime)
+                throw ConflictScheduleException("뒤 스케줄의 시작시간과 충돌합니다.")
+        }
+
+        return insertedIndex
+    }
+}
 
 data class Schedule(
     @Id
---
+++ schedule/persistence/src/main/kotlin/vp/togedo/util/exception/InvalidTimeException.kt
@@ -0,0 +1,3 @@
+package vp.togedo.util.exception
+
+class InvalidTimeException(message: String) : RuntimeException(message)
