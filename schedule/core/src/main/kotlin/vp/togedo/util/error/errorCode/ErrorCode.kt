b381a29203e1eb35e860dc6b7ea8eaab8ca85105
Seungkyu-Han <trust1204@gmail.com>
2024-12-16 23:10:38 +0900

    TEST: fixed personal schedule service test

--- schedule/core/src/main/kotlin/vp/togedo/util/error/errorCode/ErrorCode.kt
+++ schedule/core/src/main/kotlin/vp/togedo/util/error/errorCode/ErrorCode.kt
@@ -8,5 +8,9 @@ enum class ErrorCode(
 ) {
     INVALID_TOKEN(HttpStatus.FORBIDDEN, "유효하지 않은 토큰입니다."),
 
-    SCHEDULE_INFO_CANT_FIND(HttpStatus.NOT_FOUND, "해당 유저의 스케줄 정보가 존재하지 않습니다.")
+    SCHEDULE_INFO_CANT_FIND(HttpStatus.NOT_FOUND, "해당 유저의 스케줄 정보가 존재하지 않습니다."),
+    SCHEDULE_CONFLICT(HttpStatus.CONFLICT, "스케줄의 시간이 충돌합니다."),
+    END_TIME_BEFORE_START_TIME(HttpStatus.BAD_REQUEST, "스케줄의 종료시간이 시작시간보다 빠릅니다."),
+    BAD_SCHEDULE_TIME(HttpStatus.BAD_REQUEST, "스케줄의 종료시간이 시작시간보다 빠릅니다."),
+    SCHEDULE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 스케줄이 존재하지 않습니다")
 }
