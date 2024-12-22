e6f6f586146fb167e445e5fa17b9b053fa3e0798
Seungkyu-Han <trust1204@gmail.com>
2024-12-22 14:55:42 +0900

    REFACTOR: change parameter name

--- schedule/presentation/src/main/kotlin/vp/togedo/data/dto/flexiblePersonalSchedule/FlexiblePersonalScheduleListDto.kt
+++ schedule/presentation/src/main/kotlin/vp/togedo/data/dto/flexiblePersonalSchedule/FlexiblePersonalScheduleListDto.kt
@@ -4,5 +4,5 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 data class FlexiblePersonalScheduleListDto(
     @JsonProperty("flexibleSchedules")
-    val schedules: List<FlexiblePersonalScheduleDto>
+    val flexibleSchedules: List<FlexiblePersonalScheduleDto>
 )
