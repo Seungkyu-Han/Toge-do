f666bcdf316d12190306bd9275702e783078af6d
Seungkyu-Han <trust1204@gmail.com>
2024-12-23 18:30:12 +0900

    ENH: add joined group document

---
+++ schedule/persistence/src/main/kotlin/vp/togedo/document/JoinedGroupDocument.kt
@@ -0,0 +1,30 @@
+package vp.togedo.document
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import org.bson.types.ObjectId
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import reactor.core.publisher.Mono
+
+@Document(collection = "joined_group")
+data class JoinedGroupDocument(
+    @Id
+    @JsonProperty("id")
+    val id: ObjectId,
+
+    val groups: MutableList<ObjectId> = mutableListOf()
+){
+    fun addGroup(id: ObjectId): Mono<JoinedGroupDocument> {
+        return Mono.fromCallable {
+            this.groups.add(id)
+            this
+        }
+    }
+
+    fun removeGroup(id: ObjectId): Mono<JoinedGroupDocument> {
+        return Mono.fromCallable {
+            this.groups.remove(id)
+            this
+        }
+    }
+}
