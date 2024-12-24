dd660d37e4f74e2eebbbda7ec0772954ad40a01a
Seungkyu-Han <trust1204@gmail.com>
2024-12-24 18:54:55 +0900

    ENH: create group API

--- schedule/presentation/src/main/kotlin/vp/togedo/controller/GroupController.kt
+++ schedule/presentation/src/main/kotlin/vp/togedo/controller/GroupController.kt
@@ -7,15 +7,15 @@ import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestHeader
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 import vp.togedo.config.IdComponent
 import vp.togedo.connector.GroupConnector
+import vp.togedo.data.dto.group.CreateGroupReqDto
 import vp.togedo.data.dto.group.GroupDto
 
 @RestController
@@ -40,6 +40,24 @@ class GroupController(
         return ResponseEntity.ok().body(groupConnector.readGroups(
             userId = idComponent.objectIdProvider(userId)
         ))
+    }
+
+    @PostMapping("/create")
+    @Operation(summary = "그룹을 생성")
+    @ApiResponses(
+        ApiResponse(responseCode = "201", description = "OK",
+            content = [Content(schema = Schema(implementation = HttpStatus::class))]),
+        ApiResponse(responseCode = "403", description = "권한 에러",
+            content = [Content(mediaType = MediaType.TEXT_PLAIN_VALUE)])
+    )
+    fun createGroup(
+        @Parameter(hidden = true) @RequestHeader("X-VP-UserId") userId: String,
+        @RequestBody createGroupReqDto: CreateGroupReqDto
+    ): Mono<ResponseEntity<HttpStatus>> {
+        return groupConnector.createGroup(
+            userId = idComponent.objectIdProvider(userId),
+            createGroupReqDto = createGroupReqDto
+        ).then(Mono.just(ResponseEntity.status(201).build()))
     }
 
 
