23f49151286a163100ca53e087bf0d4e42f99234
Seungkyu-Han <trust1204@gmail.com>
2024-12-26 17:22:33 +0900

    TEST: update group when group is exist

--- schedule/core/src/test/kotlin/vp/togedo/service/impl/GroupServiceImplTest.kt
+++ schedule/core/src/test/kotlin/vp/togedo/service/impl/GroupServiceImplTest.kt
@@ -644,4 +644,45 @@ class GroupServiceImplTest{
             verify(joinedGroupRepository, times(0)).save(any())
         }
     }
+
+    @Nested
+    inner class UpdateGroup{
+        private lateinit var groupDocument: GroupDocument
+
+        @BeforeEach
+        fun setUp() {
+            groupDocument = GroupDocument(
+                name = UUID.randomUUID().toString()
+            )
+        }
+
+        @Test
+        @DisplayName("존재하는 그룹의 이름을 수정")
+        fun updateGroupFromExistGroupReturnSuccess(){
+            //given
+            val originalName = groupDocument.name
+            val groupDao = GroupDao(
+                id = groupDocument.id,
+                name = UUID.randomUUID().toString(),
+                members = mutableListOf()
+            )
+
+            `when`(groupRepository.findById(groupDocument.id))
+                .thenReturn(Mono.just(groupDocument))
+
+            `when`(groupRepository.save(groupDocument))
+                .thenReturn(Mono.just(groupDocument))
+
+            //when
+            StepVerifier.create(groupServiceImpl.updateGroup(groupDao))
+                .expectNext(groupDao).verifyComplete()
+
+            //then
+            Assertions.assertEquals(groupDao.name, groupDocument.name)
+            Assertions.assertNotEquals(originalName, groupDocument.name)
+
+            verify(groupRepository, times(1)).findById(groupDocument.id)
+            verify(groupRepository, times(1)).save(groupDocument)
+        }
+    }
 }
