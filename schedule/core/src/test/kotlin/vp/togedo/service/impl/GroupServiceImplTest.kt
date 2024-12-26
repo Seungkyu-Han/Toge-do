d2913e4cfc3fddc0704c5e29311961e693793b5d
Seungkyu-Han <trust1204@gmail.com>
2024-12-26 17:27:21 +0900

    TEST: update group when group is not exist

--- schedule/core/src/test/kotlin/vp/togedo/service/impl/GroupServiceImplTest.kt
+++ schedule/core/src/test/kotlin/vp/togedo/service/impl/GroupServiceImplTest.kt
@@ -684,5 +684,30 @@ class GroupServiceImplTest{
             verify(groupRepository, times(1)).findById(groupDocument.id)
             verify(groupRepository, times(1)).save(groupDocument)
         }
+
+        @Test
+        @DisplayName("존재하지 않는 그룹의 이름을 수정")
+        fun updateGroupFromNotExistGroupReturnException(){
+            //given
+            val groupDao = GroupDao(
+                id = groupDocument.id,
+                name = UUID.randomUUID().toString(),
+                members = mutableListOf()
+            )
+
+            `when`(groupRepository.findById(groupDocument.id))
+                .thenReturn(Mono.empty())
+
+            //when
+            StepVerifier.create(groupServiceImpl.updateGroup(groupDao))
+                .expectErrorMatches {
+                    it is GroupException && it.errorCode == ErrorCode.NOT_EXIST_GROUP
+                }
+                .verify()
+
+            //then
+            verify(groupRepository, times(1)).findById(groupDocument.id)
+            verify(groupRepository, times(0)).save(groupDocument)
+        }
     }
 }
