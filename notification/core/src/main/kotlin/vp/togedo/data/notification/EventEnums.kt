79cd23eee192e5935241cb5428e37b8116c613fd
Seungkyu-Han <trust1204@gmail.com>
2024-12-27 18:08:24 +0900

    ENH: listen create group schedule event and send notification

--- notification/core/src/main/kotlin/vp/togedo/data/notification/EventEnums.kt
+++ notification/core/src/main/kotlin/vp/togedo/data/notification/EventEnums.kt
@@ -10,6 +10,10 @@ enum class EventEnums(
     APPROVE_FRIEND_EVENT(1, "상대방이 친구 요청을 수락했습니다.", "님이 친구 요청을 수락하였습니다."),
 
     //GROUP
-    INVITE_GROUP(2, "그룹에 초대되었습니다.", "의 그룹에 초대되었습니다.")
+    INVITE_GROUP(2, "그룹에 초대되었습니다.", "의 그룹에 초대되었습니다."),
+
+    //GROUP SCHEDULE
+    CREATE_GROUP_SCHEDULE(3, "공유 일정이 생성되었습니다.", "공유 일정이 생성되었습니다."),
+
 
 }
---
+++ notification/core/src/main/kotlin/vp/togedo/listener/GroupScheduleEventListener.kt
@@ -0,0 +1,37 @@
+package vp.togedo.listener
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.stereotype.Component
+import vp.togedo.data.groupSchedule.CreateGroupScheduleEventDto
+import vp.togedo.data.notification.EventEnums
+import vp.togedo.data.notification.SSEDao
+import vp.togedo.service.FCMService
+import vp.togedo.service.NotificationService
+
+@Component
+class GroupScheduleEventListener(
+    private val notificationService: NotificationService,
+    private val fcmService: FCMService,
+    private val objectMapper: ObjectMapper
+) {
+
+    @KafkaListener(topics = ["CREATE_GROUP_SCHEDULE_TOPIC"], groupId = "seungkyu")
+    fun createGroupSchedule(message: String){
+        val event = EventEnums.CREATE_GROUP_SCHEDULE
+        val createGroupScheduleEventDto = objectMapper.readValue(message, CreateGroupScheduleEventDto::class.java)
+        val isSSE = notificationService.publishNotification(
+            id = createGroupScheduleEventDto.receiverId,
+            sseDao = SSEDao(event, createGroupScheduleEventDto.name, null)
+        )
+        if (!isSSE){
+            fcmService.pushNotification(
+                userId = createGroupScheduleEventDto.receiverId,
+                title = event.eventTitle,
+                content = "${createGroupScheduleEventDto.name}${event.eventContent}",
+                image = null
+            )
+        }
+    }
+
+}
