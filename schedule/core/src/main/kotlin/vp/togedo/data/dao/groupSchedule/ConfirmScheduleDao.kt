0e289305bfa7316f439b711039870578b7ef9a58
Seungkyu-Han <trust1204@gmail.com>
2025-01-01 10:47:53 +0900

    ENH: confirm schedule dao

---
+++ schedule/core/src/main/kotlin/vp/togedo/data/dao/groupSchedule/ConfirmScheduleDao.kt
@@ -0,0 +1,10 @@
+package vp.togedo.data.dao.groupSchedule
+
+import org.bson.types.ObjectId
+
+data class ConfirmScheduleDao(
+    val startTime: String,
+    val endTime: String,
+    val state: GroupScheduleStateDaoEnum,
+    val confirmedUser: List<ObjectId>?
+)
