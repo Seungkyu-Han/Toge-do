13101353fce1fc9d137048dd7336a11a6fed84a1
Seungkyu-Han <trust1204@gmail.com>
2025-01-01 16:21:20 +0900

    ENH: add chat module

---
+++ chat/core/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.3.5"
+    id("io.spring.dependency-management") version "1.1.6"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(project(":chat:persistence"))
+
+    //WEBFLUX
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+    //MONGODB
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
+
+    //TEST
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.3")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
+    testImplementation("io.projectreactor:reactor-test")
+    testImplementation ("org.mockito:mockito-core")
+
+    //COROUTINE
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-rx3")
+
+    //REFLECT
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
