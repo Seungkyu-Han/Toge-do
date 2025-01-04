8ee5a9869ad0ff750731189fb925208c85eb9c1c
Seungkyu-Han <trust1204@gmail.com>
2025-01-04 12:36:46 +0900

    DOCS: publish kafka event java doc

--- kafka/src/main/kotlin/vp/togedo/kafka/publish/KafkaPublisher.kt
+++ kafka/src/main/kotlin/vp/togedo/kafka/publish/KafkaPublisher.kt
@@ -5,5 +5,11 @@ import vp.togedo.kafka.data.enums.EventEnums
 
 interface KafkaPublisher {
 
+    /**
+     * 해당 토픽으로 kafka 이벤트를 전송하는 메서드
+     * @param eventEnums 해당 이벤트의 정보
+     * @param eventClass 메시지에 넣을 class
+     * @return mono void
+     */
     fun publishKafkaEvent(eventEnums: EventEnums, eventClass: Any): Mono<Void>
 }
