d3dafa644650c17cf79d386a01ff80a2eceec29d
Seungkyu-Han <trust1204@gmail.com>
2025-01-04 11:19:48 +0900

    ENH: add kafka module dependencies

--- kafka/build.gradle.kts
+++ kafka/build.gradle.kts
@@ -13,7 +13,17 @@ repositories {
 }
 
 dependencies {
+    //WEBFLUX
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
 
+    //KAFKA
+    implementation("io.projectreactor.kafka:reactor-kafka")
+    implementation("org.springframework.kafka:spring-kafka")
+
+    //COROUTINE
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-rx3")
 }
 
 tasks.test {
