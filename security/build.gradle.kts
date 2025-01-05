fd82a6bf8a7e85d1441825c977d59c55a1e19ba0
Seungkyu-Han <trust1204@gmail.com>
2025-01-05 20:40:40 +0900

    ENH: implementation in model module

--- security/build.gradle.kts
+++ security/build.gradle.kts
@@ -17,6 +17,14 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt:0.9.1")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("javax.xml.bind:jaxb-api:2.4.0-b180830.0359")
+
+    //TEST
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.3")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
+    testImplementation("io.projectreactor:reactor-test")
+    testImplementation ("org.mockito:mockito-core")
 }
 
 tasks.test {
