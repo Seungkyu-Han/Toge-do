c1773efe95d7daf38f3a0fa5f5b2de34dc2cc024
Seungkyu-Han <trust1204@gmail.com>
2025-01-05 10:14:18 +0900

    ENH: add test dependencies in kafka module

--- kafka/build.gradle.kts
+++ kafka/build.gradle.kts
@@ -24,6 +24,14 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-rx3")
+
+    //TEST
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.3")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
+    testImplementation("io.projectreactor:reactor-test")
+    testImplementation ("org.mockito:mockito-core")
 }
 
 tasks.test {
