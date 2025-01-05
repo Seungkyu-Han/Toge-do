619da8aa6d05602d88f2e6ef6e0093a331d03458
Seungkyu-Han <trust1204@gmail.com>
2025-01-05 22:04:23 +0900

    REFACTOR: add user document function

--- model/src/main/kotlin/vp/togedo/model/documents/user/UserDocument.kt
+++ model/src/main/kotlin/vp/togedo/model/documents/user/UserDocument.kt
@@ -4,8 +4,7 @@ import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
-import vp.togedo.model.exception.user.AlreadyFriendException
-import vp.togedo.model.exception.user.CantRequestToMeException
+import vp.togedo.model.exception.user.*
 
 @Document(collection = "users")
 data class UserDocument(
@@ -27,4 +26,127 @@ data class UserDocument(
     var friendRequests: MutableSet<ObjectId> = mutableSetOf(),
 
     var deviceToken: String? = null
-)
+){
+    /**
+     * 해당 유저에게 친구 요청
+     * @param userId 친구 요청을 보냄
+     * @return 친구 요청을 받은 사용자의 document
+     * @throws CantRequestToMeException 해당 유저가 본인인 경우
+     * @throws AlreadyFriendException 해당 친구가 이미 친구인 경우
+     * @throws AlreadyRequestFriendException 해당 친구가 이미 친구 요청을 보낸 경우
+     */
+    fun addFriendRequest(userId: ObjectId): UserDocument {
+        isNotMe(userId) && isNotFriend(userId) && isNotRequestFriend(userId)
+        this.friendRequests.add(userId)
+        return this
+    }
+
+    /**
+     * 해당 유저를 친구에 저장
+     * @param userId 친구로 저장할 유저의 object id
+     * @return 친구가 저장된 사용자의 document
+     * @throws CantRequestToMeException 해당 유저가 본인인 경우
+     * @throws AlreadyFriendException 해당 유저가 이미 친구인 경우
+     */
+    fun addFriend(userId: ObjectId): UserDocument {
+        isNotMe(userId) && isNotFriend(userId)
+        this.friends.add(userId)
+        return this
+    }
+
+    /**
+     * 해당 유저의 친구 요청을 승인
+     * @param userId 친구 요청을 승인할 유저의 object id
+     * @return 친구가 저장된 사용자의 document
+     */
+    fun approveFriendRequest(userId: ObjectId): UserDocument {
+        isNotMe(userId) && isNotFriend(userId) && isRequestFriend(userId)
+        this.friendRequests.remove(userId)
+        this.friendRequests.add(userId)
+        return this
+    }
+
+    /**
+     * 해당 유저를 친구 목록에서 삭제
+     * @param userId 친구 목록에서 삭제할 유저의 object id
+     * @return 변경된 사용자의 document
+     * @throws CantRequestToMeException 해당 유저가 본인인 경우
+     */
+    fun removeFriend(userId: ObjectId): UserDocument {
+        isNotMe(userId) && isNotFriend(userId)
+        this.friends.remove(userId)
+        return this
+    }
+
+    /**
+     * 해당 유저가 보낸 친구 요청을 삭제
+     * @param userId 삭제할 유저의 object id
+     * @return 변경된 사용자의 document
+     * @throws FriendRequestNotSentException
+     */
+    fun removeFriendRequest(userId: ObjectId): UserDocument {
+        isNotMe(userId) && isRequestFriend(userId)
+        this.friendRequests.remove(userId)
+        return this
+    }
+
+    /**
+     * 해당 유저가 본인이 아닌지 확인
+     * @param userId 확인할 유저의 object id
+     * @return true
+     * @throws CantRequestToMeException 해당 object id가 본인인 경우
+     */
+    private fun isNotMe(userId: ObjectId): Boolean{
+        if(userId == this.id)
+            throw CantRequestToMeException()
+        return true
+    }
+
+    /**
+     * 해당 유저가 친구가 아닌지 확인
+     * @param userId 확인할 유저의 object id
+     * @return true
+     * @throws AlreadyFriendException 해당 object id가 이미 친구인 경우
+     */
+    private fun isNotFriend(userId: ObjectId): Boolean{
+        if(friends.contains(userId))
+            throw AlreadyFriendException()
+        return true
+    }
+
+    /**
+     * 해당 유저가 친구인지 확인
+     * @param userId 확인할 유저의 object id
+     * @return true
+     * @throws NotFriendException 해당 유저가 친구가 아닌 경우
+     */
+    private fun isFriend(userId: ObjectId): Boolean{
+        if(!friends.contains(userId))
+            throw NotFriendException()
+        return true
+    }
+
+    /**
+     * 해당 유저가 친구 요청을 보낸지 않았는지 확인
+     * @param userId 확인할 유저의 object id
+     * @return true
+     * @throws AlreadyRequestFriendException 해당 유저가 친구 요청을 보냈던 경우
+     */
+    private fun isNotRequestFriend(userId: ObjectId): Boolean{
+        if(friendRequests.contains(userId))
+            throw AlreadyRequestFriendException()
+        return true
+    }
+
+    /**
+     * 해당 유저가 친구 요청을 보낸지 확인
+     * @param userId 확인할 유저의 object id
+     * @return true
+     * @throws FriendRequestNotSentException 해당 유저가 친구 요청을 보내지 않았을 경우
+     */
+    private fun isRequestFriend(userId: ObjectId): Boolean{
+        if(friendRequests.contains(userId))
+            throw FriendRequestNotSentException()
+        return true
+    }
+}
---
+++ model/src/main/kotlin/vp/togedo/model/exception/user/FriendRequestNotSentException.kt
@@ -0,0 +1,3 @@
+package vp.togedo.model.exception.user
+
+class FriendRequestNotSentException: UserException("친구 요청을 보낸 사용자가 아닙니다.")
