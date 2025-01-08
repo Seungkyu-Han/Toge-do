f4ff2548a7e0ac5b421a02e546764ddb47bd942b
Seungkyu-Han <trust1204@gmail.com>
2025-01-09 00:20:25 +0900

    TEST: add member in group test

--- model/src/test/kotlin/vp/togedo/model/documents/group/GroupDocumentTest.kt
+++ model/src/test/kotlin/vp/togedo/model/documents/group/GroupDocumentTest.kt
@@ -2,6 +2,7 @@ package vp.togedo.model.documents.group
 
 import org.bson.types.ObjectId
 import org.junit.jupiter.api.*
+import vp.togedo.model.exception.group.AlreadyMemberException
 import java.util.UUID
 
 class GroupDocumentTest{
@@ -34,6 +35,40 @@ class GroupDocumentTest{
 
             //then
             Assertions.assertEquals(newName, groupDocument.name)
+        }
+    }
+
+    @Nested
+    inner class AddMember{
+        @BeforeEach
+        fun setUp() {
+            groupDocument = GroupDocument(
+                name = UUID.randomUUID().toString(),
+                members = mutableSetOf(userId)
+            )
+        }
+
+        @Test
+        @DisplayName("해당 그룹에 없는 멤버를 추가")
+        fun addMemberToNotExistMemberReturnsSuccess(){
+            //given
+            val newMember = ObjectId.get()
+
+            //when
+            groupDocument.addMember(newMember)
+
+            //then
+            Assertions.assertTrue(groupDocument.members.contains(newMember))
+        }
+
+        @Test
+        @DisplayName("해당 그룹에 존재하는 멤버를 추가")
+        fun addMemberToExistMemberReturnException(){
+
+            //when && then
+            Assertions.assertThrows(AlreadyMemberException::class.java) {
+                groupDocument.addMember(userId = userId)
+            }
         }
     }
 }
