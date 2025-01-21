a786a318eb4daf7a17c5ef4e9054fcadd9ff4039
Seungkyu-Han <trust1204@gmail.com>
2025-01-22 00:17:50 +0900

    ENH: valid code redis service

---
+++ redis/src/main/kotlin/vp/togedo/redis/validCode/impl/ValidCodeServiceImpl.kt
@@ -0,0 +1,24 @@
+package vp.togedo.redis.validCode.impl
+
+import org.springframework.data.redis.core.ReactiveRedisTemplate
+import org.springframework.stereotype.Service
+import reactor.core.publisher.Mono
+import vp.togedo.redis.validCode.ValidCodeService
+import java.time.Duration
+
+@Service
+class ValidCodeServiceImpl(
+    private val reactiveRedisTemplate: ReactiveRedisTemplate<String, String>,
+): ValidCodeService {
+
+    private val validCodePrefix = "email:validation:"
+
+    override fun saveCodeByEmail(email: String, code: String): Mono<Void> =
+        reactiveRedisTemplate.opsForValue()
+            .set(validCodePrefix + email, code, Duration.ofMinutes(5))
+            .then()
+
+
+    override fun findCodeByEmail(email: String): Mono<String> =
+        reactiveRedisTemplate.opsForValue().get(validCodePrefix + email)
+}
